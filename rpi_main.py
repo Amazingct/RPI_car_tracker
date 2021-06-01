c7fe4d3aefbb17b1b863e68af6b95832658c22c0
amazing <amazing.comptech@gmail.com>
2021-06-01 11:52:40 +0000

    camera function settled

--- rpi_main.py
+++ rpi_main.py
@@ -1,1 +1,25 @@
-print("hello rpi")
+import cv2
+import time
+
+# Major functions
+def get_gps():
+    pass
+
+def buzz():
+    pass
+
+def capture_images(no=1, s = 1):
+    image = cv2.VideoCapture(0)
+    result = True
+    while result:
+        for n in range(no):
+            ret, frame = image.read()
+            image_name = "data/images/capture_" + str(n) + ".jpg"
+            cv2.imwrite(image_name, frame)
+            time.sleep(s)
+        result = False
+    image.release()
+    cv2.destroyAllWindows()
+
+
+capture_images(3, 2)
