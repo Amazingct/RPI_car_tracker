07d075f041c90618c50d80304ff7148f4892886d
amazing <amazing.comptech@gmail.com>
2021-06-19 11:38:12 +0000

    firebase storage

--- rpi_main.py
+++ rpi_main.py
@@ -2,32 +2,58 @@ from flask import Flask, request, Response, send_file
 import cv2
 import pyrebase, time
 import threading as t
+
 config = {
-    "apiKey": "AIzaSyBUmN6mlS0IdM9cf5rghIyU_MyZ_vz8SKU",
-    "authDomain": "rpi-car-tracker.firebaseapp.com",
-    "databaseURL": "https://rpi-car-tracker-default-rtdb.firebaseio.com",
-    "projectId": "rpi-car-tracker",
-    "storageBucket": "rpi-car-tracker.appspot.com",
-    "messagingSenderId": "67371868058",
-    "appId": "1:67371868058:web:919cb18cee9eb0a85b2423",
-    "measurementId": "G-X2PC0NSXNV"
+    "apiKey": "AIzaSyC_DC3erAG6oaHt3v9YdBUEliLm-3nd7Ds",
+    "authDomain": "cartracker-f52df.firebaseapp.com",
+    "databaseURL": "https://cartracker-f52df-default-rtdb.firebaseio.com",
+    "projectId": "cartracker-f52df",
+    "storageBucket": "cartracker-f52df.appspot.com",
+    "messagingSenderId": "697426173079",
+    "appId": "1:697426173079:web:379fa3a26d436672f71dd2",
+    "measurementId": "G-L4H00531P4",
+    "serviceAccount":"service.json"
+
 }
 
 fire_base = pyrebase.initialize_app(config)
 auth = fire_base.auth()
 database = fire_base.database()
+storage = fire_base.storage()
 
 
 # Major functions
+def capture_image():
+    image = cv2.VideoCapture(0)
+    result = True
+    while result:
+        ret, frame = image.read()
+        image_name = "data/images/snap.png"
+        cv2.imwrite(image_name, frame)
+        result = False
+    image.release()
+    cv2.destroyAllWindows()
+
+
 def get_gps():
     # get from module
     return {"lat": 33.4, "long": -66.9}
 
+
 def update_gps_firebase():
     print("Update started..")
     while True:
-        database.child("gps").update(get_gps())
+        database.child("track").update(get_gps())
         time.sleep(1)
+
+
+def update_image():
+    while True:
+        capture_image()
+        time.sleep(1)
+        storage.child("snap.png").put("data/images/snap.png")
+        time.sleep(4)
+
 
 def buzzer(state):
     if state == 1:
@@ -35,21 +61,13 @@ def buzzer(state):
     else:
         return "alarm turned off"
 
-def capture_image():
-    image = cv2.VideoCapture(0)
-    result = True
-    while result:
-        ret, frame = image.read()
-        image_name = "data/images/capture.jpg"
-        cv2.imwrite(image_name, frame)
-        result = False
-    image.release()
-    cv2.destroyAllWindows()
-
 
 t.Thread(target=update_gps_firebase).start()
+t.Thread(target=update_image).start()
 
 app = Flask(__name__)
+
+
 @app.route("/capture", methods=['GET'])
 def capture():
     capture_image()
