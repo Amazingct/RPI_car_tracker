400fee88632642597b24420fc42db85b8b4a6e7a
amazing <amazing.comptech@gmail.com>
2021-08-03 05:40:51 +0000

    firebase storage

--- rpi_main.py
+++ rpi_main.py
@@ -2,6 +2,13 @@ from flask import Flask, request, Response, send_file
 import cv2
 import pyrebase, time
 import threading as t
+import serial
+import time
+import pynmea2
+
+
+port = "/dev/ttyAMA0"
+ser = serial.Serial(port, baudrate=9600, timeout=0.5)
 
 config = {
     "apiKey": "AIzaSyC_DC3erAG6oaHt3v9YdBUEliLm-3nd7Ds",
@@ -13,7 +20,6 @@ config = {
     "appId": "1:697426173079:web:379fa3a26d436672f71dd2",
     "measurementId": "G-L4H00531P4",
     "serviceAccount":"service.json"
-
 }
 
 fire_base = pyrebase.initialize_app(config)
@@ -23,67 +29,69 @@ storage = fire_base.storage()
 
 
 # Major functions
+
 def capture_image():
     image = cv2.VideoCapture(0)
     result = True
     while result:
         ret, frame = image.read()
-        image_name = "data/images/snap.png"
+        image_name = "data/images/snap.jpeg"
         cv2.imwrite(image_name, frame)
         result = False
     image.release()
     cv2.destroyAllWindows()
+    print("image saved")
+
+
 
 
 def get_gps():
-    # get from module
-    return {"lat": 33.4, "long": -66.9}
+    dataout = pynmea2.NMEAStreamReader()
+    newdata = ser.readline()
+    newdata = newdata.decode("utf-8", "ignore")
+    if newdata[0:6] == "$GNRMC":
+        newmsg = pynmea2.parse(newdata)
+        lat = newmsg.latitude
+        lng = newmsg.longitude
+        gps = {"lat": str(lat), "long": str(lng)}
+        print(gps)
+        return gps
+    else:
+        return 0
 
 
 def update_gps_firebase():
-    print("Update started..")
     while True:
-        database.child("track").update(get_gps())
-        time.sleep(1)
-
+        data = get_gps()
+        if data != 0:
+            database.child("track").update(data)
 
 def update_image():
     while True:
         capture_image()
         time.sleep(1)
-        storage.child("snap.png").put("data/images/snap.png")
+        storage.child("snap1.png").put("data/images/snap.png")
         time.sleep(4)
+        print("image sent")
+
+def alarm(state):
+    pass
+
+def sos():
+    pass
+
+def read_alarm_sos():
+    while True:
+        alarm_sos = database.child("track").get().val()
+        alarm_sos = [alarm_sos["alarm"], alarm_sos["sos"]]
+        print(alarm_sos)
+        alarm(alarm_sos["alarm"])
+        if alarm_sos["sos"] == 1:
+            sos()
 
 
-def buzzer(state):
-    if state == 1:
-        return "alarm turned on"
-    else:
-        return "alarm turned off"
 
 
-t.Thread(target=update_gps_firebase).start()
+#t.Thread(target=update_gps_firebase).start()
 t.Thread(target=update_image).start()
-
-app = Flask(__name__)
-
-
-@app.route("/capture", methods=['GET'])
-def capture():
-    capture_image()
-    return send_file("data/images/capture.jpg")
-
-
-@app.route("/alarm", methods=['POST'])
-def alarm():
-    state = request.args.get('alarm')
-    return buzzer(int(state))
-
-
-@app.route("/")
-def home():
-    return "RPI CAR TRACKER"
-
-
-if __name__ == '__main__':
-    app.run()
+#t.Thread(target=read_alarm_sos).start()
